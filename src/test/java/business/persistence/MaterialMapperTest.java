6485f92e35f1e8228adb1155a81441d4fc8154f6
Janussr <Janusstivang@gmail.com>
2021-05-22 14:48:28 +0200

    idk

--- src/test/java/business/persistence/MaterialMapperTest.java
+++ src/test/java/business/persistence/MaterialMapperTest.java
@@ -1,10 +1,12 @@
 package business.persistence;
 
 import business.entities.BomLine;
+import business.entities.Material;
 import business.entities.Order;
 import business.entities.User;
 import business.exceptions.UserException;
 import business.services.BomService;
+import business.services.MaterialFacade;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -37,6 +39,7 @@ class MaterialMapperTest {
             materialMapper = new MaterialMapper(database);
             bomService = new BomService(database);
             orderMapper = new OrderMapper(database);
+
         } catch (ClassNotFoundException e) {   // kan ikke finde driveren i database klassen
             fail("Database connection failed. Missing jdbc driver");
         }
@@ -47,37 +50,56 @@ class MaterialMapperTest {
         // reset test database
         try (Statement stmt = database.connect().createStatement()) {
 
-            //Placer
+            //Placed like this bcus of foregin key error with "Drop table if exists"
             stmt.execute("delete from bom_items");
             stmt.execute("delete from orders");
             stmt.execute("delete from user");
+            stmt.execute("delete from material");
 
+            //Creates 3 users, dont think i need this.
             stmt.execute(
                     "insert into user values " +
                             "(1,'jens@somewhere.com','jensen','customer'), " +
                             "(2,'ken@somewhere.com','kensen','customer'), " +
                             "(3,'robin@somewhere.com','batman','employee')");
 
+            //Create three materials in DB
             stmt.execute(
-                    "insert into orders values " +
-                            "(1,1, '250','bekræftet','240','240','2021-05-15'), " +
-                            "(2,2, '300','bekræftet','140','740','2021-05-14'), " +
-                            "(3,3, '750','bekræftet','540','640','2021-05-18')");
+                    "insert into material values " +
+                            "(1, '25x200 mm. trykimp. Brædt', 'Stk', '100', 'wood'), " +
+                            "(2, '25x125mm. trykimp. Brædt', 'Stk', '75', 'wood'), " +
+                            "(3, '38x73 mm. Lægte ubh.', 'Stk', '115', 'wood')");
+
         } catch (SQLException ex) {
             System.out.println("Could not open connection to database: " + ex.getMessage());
         }
     }
 
+    //Test if it adds a material in db(from 3 to 4)
     @Test
     void addMaterial() throws UserException {
-        Timestamp orderDate = Timestamp.valueOf("2021-05-21 00:00:00");
-        Order order = new Order(1, 1, orderDate, 544.5, "bekræftet");
-        order.setOrderId(new User(1, "jens@somewhere.com", "jensen", "customer"));
-        List<BomLine> bomLines = bomService.calculateCarportFromMeasurements(300, 700);
-        orderMapper.createOrder(1, 255, 244, bomLines);
-        List<Order> orderList = orderMapper.getAllOrdersByUserId(1);
-        assertEquals(2, orderList.size());
-
+        Material material = new Material(1, "langt træ", "stk", 25.2, "wood");
+        materialMapper.addMaterial(material);
+        List<Material> materialList = materialMapper.getAllMaterials();
+        assertNotNull(materialList);
+        assertEquals(4,materialList.size());
 
     }
+    @Test
+    void getAllMaterials() {
+    }
+
+    @Test
+    void updateMaterialById() {
+    }
+
+    @Test
+    void deleteMaterial() throws UserException {
+       Material tmp = materialMapper.getMaterialById(1);
+        materialMapper.deleteMaterial(1);
+        List<Material> materialList = materialMapper.getAllMaterials();
+        assertEquals(2,materialList.size());
+    }
+
+
 }
