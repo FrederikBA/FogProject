0bf8874fbd0aee8eab1487b892083abaad694d10
Janussr <Janusstivang@gmail.com>
2021-05-25 20:08:47 +0200

    fix test so none fails

--- src/test/java/business/persistence/OrderMapperTest.java
+++ src/test/java/business/persistence/OrderMapperTest.java
@@ -17,11 +17,11 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.*;
 
 class OrderMapperTest {
-    private final static String DATABASE = "fog_carport";  // Change this to your own database
-    private final static String TESTDATABASE = DATABASE + "_test";
-    private final static String USER = "root";
-    private final static String PASSWORD = "root123";
-    private final static String URL = "jdbc:mysql://localhost:3306/" + TESTDATABASE + "?serverTimezone=CET&useSSL=false";
+    private final static String DATABASE = "fog_carport" ;  // Change this to your own database
+    private final static String TESTDATABASE = DATABASE + "_test" ;
+    private final static String USER = "root" ;
+    private final static String PASSWORD = "root" ;
+    private final static String URL = "jdbc:mysql://localhost:3306/" + TESTDATABASE + "?serverTimezone=CET&useSSL=false" ;
 
     private static Database database;
     private static UserMapper userMapper;
@@ -51,6 +51,7 @@ class OrderMapperTest {
             stmt.execute("delete from bom_items");
             stmt.execute("delete from orders");
             stmt.execute("delete from user");
+            stmt.execute("delete from material");
             stmt.execute(
                     "insert into user values " +
                             "(1,'jens@somewhere.com','jensen','customer'), " +
@@ -62,6 +63,31 @@ class OrderMapperTest {
                             "(1,1, '250','bekræftet','240','240','2021-05-15'), " +
                             "(2,2, '300','bekræftet','140','740','2021-05-14'), " +
                             "(3,3, '750','bekræftet','540','640','2021-05-18')");
+            stmt.execute(
+                    "insert into material values " +
+                            "(1, '25x200 mm. trykimp. Brædt', 'Stk', '100', 'wood'), " +
+                            "(2, '25x125mm. trykimp. Brædt', 'Stk', '100', 'wood'), " +
+                            "(3, '38x73 mm. Lægte ubh.', 'Stk', '100', 'wood')," +
+                            "(4, '45x95 mm. Reglar ub.', 'Stk', 105, 'wood')," +
+                            "(5, '45x195 mm. spærtræ ubh.', 'Stk', 250, 'wood')," +
+                            "(6, '97x97 mm. trykimp. Stolpe', 'Stk', 100, 'wood')," +
+                            "(7, '19x100 mm. trykimp. Brædt', 'Stk', 125, 'wood')," +
+                            "(8, 'Plastmo Ecolite blåtonet', 'Stk', 145, 'wood')," +
+                            "(9, 'plastmo bundskruer 200 stk.', 'Pakke', 75, 'accesories')," +
+                            "(10, 'hulbånd 1x20 mm. 10 mtr.', 'Rulle', 50, 'accesories')," +
+                            "(11, 'universal 190 mm højre', 'Stk', 30, 'accesories')," +
+                            "(12, 'universal 190 mm venstre', 'Stk', 30, 'accesories')," +
+                            "(13, '4,5 x 60 mm. skruer 200 stk.', 'Pakke', 75, 'accesories')," +
+                            "(14, '4,0 x 50 mm. beslagskruer 250 stk.', 'Pakke', 55, 'accesories')," +
+                            "(15, 'bræddebolt 10 x 120 mm.', 'Stk', 4, 'accesories')," +
+                            "(16, 'firkantskiver 40x40x11mm', 'Stk', 5, 'accesories')," +
+                            "(17, '4,5 x 70 mm. Skruer 400 stk.', 'Pakke', 60, 'accesories')," +
+                            "(18,'4,5 x 50 mm. Skruer 300 stk.', 'Pakke', 45, 'accesories')," +
+                            "(19, 'stalddørsgreb 50x75', 'Sæt', 35, 'accesories')," +
+                            "(20, 't hængsel 390 mm', 'Stk', 10, 'accesories')," +
+                            "(21, 'vinkelbeslag 35', 'Stk', 10, 'accesories')");
+
+
         } catch (SQLException ex) {
             System.out.println("Could not open connection to database: " + ex.getMessage());
         }
